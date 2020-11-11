d53d44c0406ae49a94eac8735078fc475c3eeda2
rasg <rodolfo@mutalis.com>
2020-11-10 20:02:38 -0600

    refactors

--- index.js
+++ index.js
@@ -3,23 +3,21 @@ const inputs = form.querySelectorAll('.inputs input');
 
 const handleInput = e => {
   // check for data that was inputtted and if there is a next input, focus it
-  const input = e.target;
+  const input = e.target
   if (input.nextElementSibling && input.value) {
-    input.nextElementSibling.focus();
+    input.nextElementSibling.focus()
   }
 }
 
 const handlePaste = e => {
-  const paste = e.clipboardData.getData('text');
+  const paste = e.clipboardData.getData('text')
   // loop over each input, and populate with the index of that string
   inputs.forEach((input, i) => {
-    console.log(input);
-    input.value = paste[i] || '';
-  });
+    console.log(input)
+    input.value = paste[i] || ''
+  })
 }
 
-inputs[0].addEventListener('paste', handlePaste);
+inputs[0].addEventListener('paste', handlePaste)
 
-form.addEventListener('input', handleInput);
-// 123456
-// 654321
+form.addEventListener('input', handleInput)
